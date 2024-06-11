8392482b7b0b88c49b21ca2f66a79f2f462e18a3
ByteTrix <agon.xhaferi@bytetrix.dev>
2024-06-11 16:19:43 +0200

    Finished adapter pattern implementation

--- src/main/kotlin/adapter_pattern/AdapternPattern.kt
+++ src/main/kotlin/adapter_pattern/AdapternPattern.kt
@@ -1,5 +1,12 @@
 package main.kotlin.adapter_pattern
 
-fun main(){
+import main.kotlin.adapter_pattern.abstract_pattern.Target
+import main.kotlin.adapter_pattern.implementation.Adaptee
+import main.kotlin.adapter_pattern.implementation.Adapter
 
+fun main() {
+    val adaptee = Adaptee()
+    val target: Target = Adapter(adaptee)
+
+    target.request()
 }
--- src/main/kotlin/adapter_pattern/implementation/Adaptee.kt
+++ src/main/kotlin/adapter_pattern/implementation/Adaptee.kt
@@ -2,6 +2,6 @@ package main.kotlin.adapter_pattern.implementation
 
 class Adaptee {
     fun specificRequest() {
-
+        println("The specific request that the adapter requires has been called.")
     }
 }
